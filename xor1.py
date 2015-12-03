bc2371abb4007348465dbd7dcf6220599474f4db
Adam Szlachta <adam.szlachta@gmail.com>
2015-12-03 12:54:10 +0100

    example xor

---
+++ xor1.py
@@ -0,0 +1,22 @@
+from pybrain.datasets import SupervisedDataSet
+from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.structure.modules import TanhLayer
+
+net = buildNetwork(2, 3, 1, bias=True)
+ds = SupervisedDataSet(2, 1)
+ds.addSample((0, 0), (0,))
+ds.addSample((0, 1), (1,))
+ds.addSample((1, 0), (1,))
+ds.addSample((1, 1), (0,))
+
+# trainer = BackpropTrainer(net, ds)
+# trainer.trainUntilConvergence()
+
+trainer = BackpropTrainer(net, ds, learningrate = 0.9, momentum=0.0, weightdecay=0.0, verbose=True)
+trainer.trainEpochs(epochs=100)
+
+print '0,0->', net.activate([0,0])
+print '0,1->', net.activate([0,1])
+print '1,0->', net.activate([1,0])
+print '1,1->', net.activate([1,1])
